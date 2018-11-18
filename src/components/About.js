217ff17590deee9584198374707986ea2ebda7e8
samuel <samueldcabral@gmail.com>
2018-11-17 23:25:02 -0300

    second commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi alias perferendis hic, fugiat corporis repellat sunt praesentium, quis at eum sit non animi laudantium accusamus magni recusandae nihil quae facere.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = (props) => {
+  // setTimeout(() => {
+  //   props.history.push('/about')
+  // }, 2000);
+
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi alias perferendis hic, fugiat corporis repellat sunt praesentium, quis at eum sit non animi laudantium accusamus magni recusandae nihil quae facere.</p>
+    </div>
+  )
+}
+
+export default Contact;
